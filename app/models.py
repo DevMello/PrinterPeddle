8e5cbf3f011669f7999f3f1069457e254688e0b2
yotam <spacecat@drizzle.dubiner.org>
2022-09-24 21:20:36 -0700

    backend shit

--- app/models.py
+++ app/models.py
@@ -8,4 +8,13 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(1000))
     phone = db.Column(db.Integer)
 
+class Seller(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(1000))
+    password = db.Column(db.String(1000))
+    name = db.Column(db.String(1000))
+    username = db.Column(db.String(1000), unique=True)
+    phone = db.Column(db.Integer)
+    best_printer = []
+
 
